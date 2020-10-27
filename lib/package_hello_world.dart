67cb9280b28e06204c96f966d9fb4fe75568af21
Dimitri Leurs <dimitri.leurs@numberly.com>
2020-10-27 18:17:47 +0100

    New package

--- lib/package_hello_world.dart
+++ lib/package_hello_world.dart
@@ -1,7 +1,16 @@
 library package_hello_world;
 
+import 'package:flutter/material.dart';
+
 /// A Calculator.
 class MyCalculator {
   /// Returns [value] plus 1.
   int addOne(int value) => value + 1;
+}
+
+Widget helloWorld() {
+  return Text(
+    "Hello World",
+    style: TextStyle(fontSize: 30),
+  );
 }
